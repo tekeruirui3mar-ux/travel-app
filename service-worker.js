ff61c307c960b76e835d5000cf8bf1725fef9bf4
tekeruirui3mar-ux <tekeruirui.3.mar@gmail.com>
2026-06-11 17:04:11 +0900

    Create service-worker.js
    オフライン環境でもアプリを起動できるように、リソースをキャッシュするファイルです。

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'kanji-app-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
